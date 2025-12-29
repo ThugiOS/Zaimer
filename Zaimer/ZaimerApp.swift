fd7c89f335db19f28ea06d68ba519f51495407cc
artem <itnikitins@gmail.com>
2025-12-29 10:42:28 +0300

    initial commit

---
+++ Zaimer/ZaimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZaimerApp.swift
+//  Zaimer
+//
+//  Created by Никитин Артем on 28.12.25.
+//
+
+import SwiftUI
+
+@main
+struct ZaimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
