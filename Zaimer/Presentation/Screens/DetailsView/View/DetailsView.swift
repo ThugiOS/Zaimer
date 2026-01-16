8eae447eb191fe48e42afa80dfa6436683383900
artem <itnikitins@gmail.com>
2026-01-16 23:15:44 +0300

    details view done

--- Zaimer/Presentation/Screens/DetailsView/View/DetailsView.swift
+++ Zaimer/Presentation/Screens/DetailsView/View/DetailsView.swift
@@ -8,32 +8,114 @@
 import SwiftUI
 
 struct DetailsView: View {
+    @State var isNotificationEnabled: Bool = false
+    
     var body: some View {
         VStack(alignment: .leading) {
             header
+            
             VStack(alignment: .leading) {
-                VStack(alignment: .leading) {
-                    Text("$1299")
-                        .font(.custom(MonoNovaFont.main.rawValue, size: 28))
-                        .foregroundStyle(.mainDirtyFFFF)
-                    Text("iPhone credit")
-                        .font(.custom(MonoNovaFont.main.rawValue, size: 18))
-                        .foregroundStyle(.gray)
+                VStack(alignment: .leading, spacing: 20) {
+                    VStack(alignment: .leading) {
+                        Text("$1299")
+                            .font(.custom(MonoNovaFont.main.rawValue, size: 28))
+                            .foregroundStyle(.mainDirtyFFFF)
+                        Text("iPhone credit")
+                            .font(.custom(MonoNovaFont.main.rawValue, size: 18))
+                            .foregroundStyle(.gray)
+                    }
+                    .padding(.vertical, 20)
+                    
+                    VStack(alignment: .leading, spacing: 20) {
+                        HStack(spacing: 20) {
+                            TagInfoView(text: "23e23")
+                            TagInfoView(text: "ddwd")
+                        }
+                        
+                        Text("some text ome textome kds cks dkc skd c sdc jkds kc kjsd jksdjk jk sj cdjc kjs dcj sdjk cjk ds ckj sdc jsd kc ksjd ckj sd cjs dcj jsd c sdjc  text ome text ome text ome text")
+                            .font(.custom(MonoNovaFont.main.rawValue, size: 18))
+                            .foregroundStyle(.gray)
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 20) {
+                        Divider()
+                            .background(.mainDirtyFFFF)
+                        
+                        HStack {
+                            Text("Ближайший платеж")
+                                .font(.custom(MonoNovaFont.main.rawValue, size: 18))
+                                .foregroundStyle(.gray)
+                            Spacer()
+                            HStack(spacing: 2) {
+                                Text("оплачен:")
+                                    .font(.custom(MonoNovaFont.main.rawValue, size: 18))
+                                    .foregroundStyle(.gray)
+                                Text("01.07")
+                                    .font(.custom(MonoNovaFont.main.rawValue, size: 18))
+                                    .foregroundStyle(.white)
+                            }
+                        }
+                        Divider()
+                            .background(.mainDirtyFFFF)
+                        
+                        HStack {
+                            Text("Уведомления о платеже ")
+                                .font(.custom(MonoNovaFont.main.rawValue, size: 18))
+                                .foregroundStyle(.gray)
+                            Spacer()
+                            //toggle
+                            RadioButton(isOn: $isNotificationEnabled)
+                        }
+                    }
+                    
                 }
-                .padding(.vertical , 40)
             }
-            HStack(spacing: 20) {
-                TagInfoView(text: "23e23")
-                TagInfoView(text: "ddwd")
-            }
+            
             Spacer()
+            
+            HStack {
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Text("Оплатить")
+                        .font(.custom(MonoNovaFont.main.rawValue, size: 18))
+                        .foregroundStyle(.mainTiffany)
+                }
+                .padding(.horizontal, 50)
+                .padding(.top, 12)
+                .padding(.bottom, 10)
+                .overlay {
+                    Capsule()
+                        .stroke(.mainTiffany,lineWidth: 5)
+                }
+                
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Text("dwed")
+                        .font(.custom(MonoNovaFont.main.rawValue, size: 18))
+                        .foregroundStyle(.mainTiffany)
+                }
+                .padding(.horizontal, 50)
+                .padding(.top, 12)
+                .padding(.bottom, 10)
+                .overlay {
+                    Capsule()
+                        .stroke(.mainTiffany,lineWidth: 5)
+                }
+                
+                Spacer()
+            }
+            
         }
         .padding(.horizontal, 20)
         .background(.mainBlack)
-        
-        
+
     }
-    
 }
 
 
@@ -42,6 +124,7 @@ struct DetailsView: View {
 }
 
 extension DetailsView {
+
     var header: some View {
         HStack {
             Button {
@@ -89,6 +172,24 @@ struct TagInfoView: View {
             .overlay {
                 Capsule()
                     .stroke(.mainTiffany,lineWidth: 2)
-            } 
+            }
+    }
+}
+
+struct RadioButton: View {
+    @Binding var isOn: Bool
+    
+    var body: some View {
+        Circle()
+            .fill(isOn ? Color.mainTiffany : Color.gray)
+            .frame(width: 25, height: 25 )
+            .overlay {
+                Circle()
+                    .stroke(style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round))
+                    .foregroundStyle(.mainDirtyFFFF)
+            }
+            .onTapGesture {
+                isOn.toggle()
+            }
     }
 }
