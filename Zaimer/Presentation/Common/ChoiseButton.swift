0e625c9668751ec84879804007a675e6a5ac8777
artem <itnikitins@gmail.com>
2026-01-21 15:56:32 +0300

    added choise button

---
+++ Zaimer/Presentation/Common/ChoiseButton.swift
@@ -0,0 +1,35 @@
+//
+//  ChoiseButton.swift
+//  Zaimer
+//
+//  Created by Никитин Артем on 21.01.26.
+//
+
+import SwiftUI
+
+struct ChoiseButton: View {
+    var text: String
+    var frameColor: Color
+    var textColor: Color
+    var textSize: Int
+    var isSelected: Bool
+    
+    var body: some View {
+        Button {
+            //
+        } label: {
+            Text(text)
+                .font(.custom(MonoNovaFont.main.rawValue, size: CGFloat(textSize)))
+                .padding(.top, 13)
+                .padding(.bottom, 17)
+                .frame(maxWidth: .infinity)
+                .foregroundStyle(isSelected ? .mainDirtyFFFF : .mainTiffany)
+                .background(isSelected ? .mainTiffany : .mainBlack)
+                .clipShape(Capsule())
+                .overlay {
+                    Capsule()
+                        .stroke(.mainTiffany, lineWidth: 2)
+                }
+        }
+    }
+}
